c77e739962b8e28dd06dae43cae9452f3f29c11f
DoubleThink1984 <d.a.cook1@outlook.com>
2018-12-10 16:40:52 -0600

    add about js page

---
+++ ReactDotNetCore_2_2_Sandbox/ClientApp/src/components/About.js
@@ -0,0 +1,19 @@
+﻿import React, { Component } from 'react';
+
+export class About extends Component {
+
+    //constructor() { }
+
+    static displayName = About.name;
+
+    render() {
+        return (
+            <div>
+                <div>
+                    This is the About page.
+            </div>
+                <div>And this is another div.</div>
+            </div>            
+        );
+    }
+}
